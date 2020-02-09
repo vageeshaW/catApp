3e91c879c7b60937c3abf42756771919332dbf65
vageesha <vageeshawihangi@gmail.com>
2020-02-09 22:43:36 +0530

    changes

--- app.js
+++ app.js
@@ -1,10 +1,8 @@
 const express = require('express');
 const bodyParser = require("body-parser");
-const path = require('path');
 var cors = require('cors')
 
 var breeds = require('./routes/breeds'); 
-var routes = require('./routes');
 var app = express();
 app.use(cors())
  
@@ -15,13 +13,11 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 
-app.get('/', routes.index);
+
 app.get('/breeds', breeds.list);
 app.post('/breeds/add', breeds.add);
 app.get('/breeds/feedData', breeds.feedData);
 
-// app.get('/breeds/edit/:id', breeds.edit);
-// app.post('/breeds/edit/:id', breeds.update);
 
 app.listen(4000, function () {
     console.log('Server is running.. on Port 4000');
